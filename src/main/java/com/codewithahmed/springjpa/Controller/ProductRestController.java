37ad1c45a8ee21fc2e382283381339af6636fe78
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-07 17:36:05 +0100

    Controller

---
+++ src/main/java/com/codewithahmed/springjpa/Controller/ProductRestController.java
@@ -0,0 +1,26 @@
+package com.codewithahmed.springjpa.Controller;
+import com.codewithahmed.springjpa.model.Product;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.codewithahmed.springjpa.repo.ProductRepository;
+
+
+@RestController
+public class ProductRestController {
+	@Autowired
+	ProductRepository repository;
+	
+	@GetMapping("/products")
+	public List<Product> getProducts(){
+		return repository.findAll();
+	}
+	
+	@GetMapping("/products/{id}")
+	public Product getProduct(@PathVariable("id") int id){
+		return repository.findById(id).get();
+	}
+
+}
