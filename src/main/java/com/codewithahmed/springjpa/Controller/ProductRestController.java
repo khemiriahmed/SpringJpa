c33e616b32e238cd56a883c1524207fb5883f480
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-07 19:14:32 +0100

    @PutMapping

--- src/main/java/com/codewithahmed/springjpa/Controller/ProductRestController.java
+++ src/main/java/com/codewithahmed/springjpa/Controller/ProductRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.codewithahmed.springjpa.repo.ProductRepository;
 
@@ -13,7 +16,7 @@ public class ProductRestController {
 	@Autowired
 	ProductRepository repository;
 	
-	@GetMapping("/products")
+	@GetMapping("/products/")
 	public List<Product> getProducts(){
 		return repository.findAll();
 	}
@@ -21,6 +24,16 @@ public class ProductRestController {
 	@GetMapping("/products/{id}")
 	public Product getProduct(@PathVariable("id") int id){
 		return repository.findById(id).get();
+	}
+	
+	@PostMapping("/products/")
+	public Product createProduct(@RequestBody Product product) {
+	  return repository.save(product);	
+	}
+	
+	@PutMapping("/products/")
+	public Product updateProduct(@RequestBody Product product){
+		 return repository.save(product);	
 	}
 
 }
