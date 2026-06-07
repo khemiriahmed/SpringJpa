404010ed34a6b96c205898d1133b155a9f32aeb8
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-07 17:36:19 +0100

    model

---
+++ src/main/java/com/codewithahmed/springjpa/model/Player.java
@@ -0,0 +1,31 @@
+package com.codewithahmed.springjpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Player {
+	@Id
+	private long id;
+	private String name;
+	private int score;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
