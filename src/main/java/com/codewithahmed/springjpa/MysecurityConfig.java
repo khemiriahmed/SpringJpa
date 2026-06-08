b4769ce5afb6b004941ac87eb69a45e003df5555
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-08 06:17:14 +0100

    MysecurityConfig

---
+++ src/main/java/com/codewithahmed/springjpa/MysecurityConfig.java
@@ -0,0 +1,44 @@
+package com.codewithahmed.springjpa;
+
+
+import org.springframework.security.config.Customizer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//port org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;  
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MysecurityConfig {
+	 
+	@Bean
+	UserDetailsService  userDetailsService () {
+	  InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+	  UserDetails user = User.withUsername("ahmed").password(passwordEncoder().encode("1234")).authorities("read").build();
+	  userDetailsService.createUser(user);
+	  return userDetailsService;
+	}
+	
+	@Bean
+	 BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	 SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+	        .httpBasic(Customizer.withDefaults())
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/public/**").permitAll()
+	            .anyRequest().authenticated()
+	        );
+	    return http.build();
+	}
+
+}
